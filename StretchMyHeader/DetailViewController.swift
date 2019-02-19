4b446b9835dd0da7e44e3f3c981a717d0777e610
Kamal Maged <Kamal_Maged@yahoo.com>
2019-02-19 11:06:48 -0500

    Initial Commit

---
+++ StretchMyHeader/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  StretchMyHeader
+//
+//  Created by Kamal Maged on 2019-02-19.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
